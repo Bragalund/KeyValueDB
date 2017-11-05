eae0f350ba7c71e467fb374ec7671373c9086320
Bragalund <bakhen15@student.westerdals.no>
2017-11-05 15:18:10 +0100

    Changed name of main-sourcefile, created a folder for sourcefiles and removed some tracked files in git.

--- sourcefiles/KeyValueDB.c
+++ sourcefiles/KeyValueDB.c
@@ -1,10 +1,6 @@
-#include "headerfiles/main.h"
+#include "../headerfiles/KeyValueDB.h"
 
 int main() {
-
-// Preprocessoren setter denne verdien
-// Den setter en begrensning på antall undernoder
-
 
     typedef struct _NODE {
         char *pszName; //Peker til navn på denne noden
