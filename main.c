3191fcab35be961aedc597560bfd41197cecf436
Bragalund <bakhen15@student.westerdals.no>
2017-11-05 14:44:50 +0100

    Added struct to main-class.

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 int main() {
-    printf("Hello, World!\n");
+
+// Preprocessoren setter denne verdien
+// Den setter en begrensning på antall undernoder
+#define MAX_NODES 10
+
+    typedef struct _NODE {
+        char *pszName; //Peker til navn på denne noden
+        unsigned long ulIntVal; //unsigned long, som holde null eller en numerisk verdi
+        char *pszString;
+        struct _NODE *pnNodes[MAX_NODES];
+    } NODE;
+
+    printf("%s", "Heihei");
+
     return 0;
 }
